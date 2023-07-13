b1ba48c125af4729a65541493aab50052a22c0a7
SapkadeXoXo <SourabhSap99@gmail.com>
2023-07-13 08:28:38 +0530

    workaround To calculate Age .
    workaround To calculate Age in number of years ,number of months, number of days.ex 21 years ,4 months ,12 days Old.

---
+++ CalculateAgeInYearsAnMonths/CalculateAgeInYearsAnMonths/CalculateAge.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateAgeInYearsAnMonths
+{
+    public class CalculateAge
+    {
+        public DateTime _DOB { get; set; }
+        public CalculateAge(DateTime DOB)
+        {
+            _DOB = DOB;
+        }
+
+        public string CalculateAgeFunc()
+        {
+            // Calculate the age in (years,month,days) formate based on the current date
+            DateTime zeroTime = new DateTime(1, 1, 1);
+            DateTime curdate = DateTime.Now.ToLocalTime();
+
+            TimeSpan span = curdate - _DOB;
+
+            // because we start at year 1 for the Gregorian 
+            // calendar, we must subtract a year here.
+
+            int years = (zeroTime + span).Year - 1;
+            int months = (zeroTime + span).Month - 1;
+            int days = (zeroTime + span).Day;
+
+            string age = $"{years} Years, {months} Months, {days} Days";
+            return age;
+        }
+    }
+}
---
+++ CalculateAgeInYearsAnMonths/CalculateAgeInYearsAnMonths/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CalculateAgeInYearsAnMonths
+{
+    class Program
+    {
+     
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a date and time:");
+            Console.Write("Year: ");
+            int year = int.Parse(Console.ReadLine());
+
+            Console.Write("Month: ");
+            int month = int.Parse(Console.ReadLine());
+
+            Console.Write("Day: ");
+            int day = int.Parse(Console.ReadLine());
+
+            Console.Write("Hour: ");
+            int hour = int.Parse(Console.ReadLine());
+
+            Console.Write("Minute: ");
+            int minute = int.Parse(Console.ReadLine());
+
+            Console.Write("Second: ");
+            int second = int.Parse(Console.ReadLine());
+
+            // Creating DateTime Object with User Inputs 
+            DateTime userInput = new DateTime(year, month, day, hour, minute, second);
+
+            //Creating Instance for Calculate age class
+            CalculateAge calculateAge = new CalculateAge(userInput);
+
+            //Calculate Age After calling the function of class
+            var result = calculateAge.CalculateAgeFunc();
+
+            //printing the Age in Number Of Years ,Number of Months ,Number of Days 
+            Console.WriteLine($"Dear User You Are : {result}  Old");
+        }
+    }
+}
